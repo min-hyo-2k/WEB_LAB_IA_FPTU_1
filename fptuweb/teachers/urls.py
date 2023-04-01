f3555feaa65beaf4719397c79b98bb77a6963f40
he140409 <mrrobotforwork@gmail.com>
2023-04-01 10:35:08 +0700

    ok

--- fptuweb/teachers/urls.py
+++ fptuweb/teachers/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="teachers_register"),
     path("", views.home, name="teachers_home"),
+    path("class-list/", views.teacher_class, name="teachers_class"),
+    path("student-list/", views.student_list, name="teachers_student_list"),
 ]
